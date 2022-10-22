d1612d8d7c103224f3d99be28471873cb8a49dc3
abraham <cesar20450@cbtis75.edu.mx>
2022-10-22 13:57:40 -0500

    Actividad 2

---
+++ Actividad 1 y 2/reloj.php
@@ -0,0 +1,24 @@
+<!-- 
+    Autor:César Abraham López Aguilar.
+    Descripción:Código que te dice la fecha del día.
+    Fecha: 30 de Septiembre del 2022
+-->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+    echo '<p>';
+    echo date('28-09-2022');
+    echo '</p>';
+?>
+    
+</body>
+</html>
