a6b2e68dcc2a6ee7161e6cff2a98b52aa2faf430
abraham <cesar20450@cbtis75.edu.mx>
2022-10-22 13:57:04 -0500

    Actividad 1

---
+++ Actividad 1 y 2/hello.php
@@ -0,0 +1,24 @@
+<!-- 
+    Autor:César Abraham López Aguilar. 
+    Fecha: 30 de Septiembre del 2022.
+-->
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Hello</title>
+</head>
+<body>
+    <?php
+    echo "";
+    echo '<h1>';
+    echo 'Hola a todos';
+    echo '</h1>';
+
+    ?>
+</body>
+</html>
